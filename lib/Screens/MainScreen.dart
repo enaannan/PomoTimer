f986e1fd0daecacd79cfd153df32f1f0c9be9361
Emmanuel Annan <enaannan@gmail.com>
2020-11-02 18:46:01 +0100

    added progress screen and started build

--- lib/Screens/MainScreen.dart
+++ lib/Screens/MainScreen.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -6,17 +8,53 @@ class MainScreen extends StatelessWidget {
     return DefaultTabController(
       length: 5,
       child: Scaffold(
+        body: TabBarView(
+          children: [
+            SafeArea(
+                child: Column(
+              children: [
+                AppBar(
+                  title: Text("PomoTimer"),
+                ),
+                Text("Current Task"),
+              CircularProgressIndicator(
+                value: 0.1,
+              )
+              ,
+                Text("Stay focused for " + "timed" + " mins"),
+                ButtonBar()
+              ],
+            )),
+            SafeArea(
+                child: AppBar(
+              title: Text("rere"),
+            )),
+            
+            SafeArea(
+                child: AppBar(
+              title: Text("rere"),
+            )),
+            SafeArea(
+                child: AppBar(
+              title: Text("rere"),
+            )),
+            SafeArea(
+                child: AppBar(
+              title: Text("rere"),
+            ))
+          ],
+        ),
         bottomNavigationBar: TabBar(
+            unselectedLabelColor: Colors.grey,
+            labelColor: Colors.blue,
+            indicatorColor: Colors.transparent,
             tabs: [
               Tab(icon: Icon(Icons.alarm)),
               Tab(icon: Icon(Icons.slideshow)),
               Tab(icon: Icon(Icons.bar_chart)),
               Tab(icon: Icon(Icons.person)),
               Tab(icon: Icon(Icons.settings)),
-            ],
-          ), 
-        
-       
+            ]),
       ),
     );
   }
