83268669869e16b58933316c6373b0ceffc2feb7
Emmanuel Annan <enaannan@gmail.com>
2020-11-11 18:00:39 +0100

    updated profile screen

--- lib/Screens/ProfileScreen.dart
+++ lib/Screens/ProfileScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomotimer/UIComponents/BlueCard.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({
@@ -30,28 +31,61 @@ class _ProfileScreenState extends State<ProfileScreen> {
             margin: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(5),
-              color: Colors.blue,
+              color: Colors.white,
               boxShadow: <BoxShadow>[
-                BoxShadow(color: Colors.pink, offset: Offset(0, 5))
+                BoxShadow(
+                    blurRadius: 4,
+                    spreadRadius: 4,
+                    color: Colors.grey[200],
+                    offset: Offset(0, 5))
               ],
             ),
             child: Stack(
+              alignment: Alignment.topCenter,
               overflow: Overflow.visible,
               children: [
-                SizedBox(
-                  height: 200,
-                ), //TODO:Remove this sized box
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text("Bra Nhii"),
-                    Text("sfasfas@gmail.com"),
-                    SizedBox()
-                  ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Center(
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 60.0, bottom: 10.0),
+                              child: Text(
+                                "Bra Nhii",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Text("sfasfas@gmail.com"),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Container(
+                              height:
+                                  100, //TODO:Remove height and replace with expanded widget but it might not workl lol
+                              color: Colors.blue,
+                            ),
+                            Text(
+                                "PomoTimer is based on a pomodoro technique that allows you to concentrate from long without being fatiuged. It also makes you take brakes wicch are vital for prolonged studies"),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 35.0),
+                              child: BlueCard(
+                                  textName: 'View more', action: () {}),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
                 Positioned(
-                  left: 100,
-                  bottom: 150,
+                  
+                  bottom: 380,
                   child: ClipOval(
                     clipper: CircularClipper(),
                     child: Image.asset(
@@ -61,7 +95,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       width: 100,
                     ),
                   ),
-                )
+                ),
+                Positioned(
+                  left: 190,
+                  child: FloatingActionButton(
+                    elevation: 1,
+                    mini: true,
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.grey,
+                    child: Icon(Icons.create),
+                    onPressed: () {}))
               ],
             ),
           )
